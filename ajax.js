0a68a526f777074f4e5813c198590773e83da27b
vrunda2712 <angy.v27@gmail.com>
2017-07-10 18:56:43 -0400

    Step-1: First AJAX request

--- ajax.js
+++ ajax.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-  /* Your code goes here */
-
+  $.ajax( {
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'text',
+  });
 });
