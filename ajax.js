3a600c7a1160e8bb292e98aa4975afa9585750e4
vrunda2712 <angy.v27@gmail.com>
2017-07-10 19:00:59 -0400

    Step-2: Binding request to a click event

--- ajax.js
+++ ajax.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-  $.ajax( {
-    url: 'http://first-ajax-api.herokuapp.com/',
-    method: 'GET',
-    dataType: 'text',
+
+  var runAjaxRequest = document.getElementById('run_ajax_request');
+
+  runAjaxRequest.addEventListener('click',function(){
+    $.ajax( {
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'text',
+    });
   });
+
 });
