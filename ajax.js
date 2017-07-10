5fbe50b5865bf4c46611a5b3ef436fe9c25693e6
vrunda2712 <angy.v27@gmail.com>
2017-07-10 19:15:34 -0400

    Step-3: Using information in response

--- ajax.js
+++ ajax.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   var runAjaxRequest = document.getElementById('run_ajax_request');
+  var runPingPong = document.getElementById('run_ping_pong');
 
   runAjaxRequest.addEventListener('click',function(){
     $.ajax( {
@@ -10,4 +11,22 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
+  runPingPong.addEventListener('click',function(){
+    $.ajax( {
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      dataType: 'text',
+    }).done(function (data) {
+      console.log(data);
+
+      var element = document.createElement('div');
+      element.innerHTML = data;
+      document.querySelector('#step3456').append(element);
+      }).fail(function( jqXHR,textStatus, errorThrown){
+      console.log('INSIDE .fail()');
+      console.log(textStatus, errorThrown);
+      }).always(function(){
+      console.log('INSIDE .always()');
+    });
+  });
 });
